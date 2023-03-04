f58bbdb860e4a859dddb4ded5048386348477f3d
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-03-04 18:04:33 -0300

    chore(prisma): some bunch generated by prisma

---
+++ src/infra/shared/database/prisma/migrations/20230304210304_jobs_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Job` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `company` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `new` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
